35cd323e235edf43a57920f7ecdb49daaa53c5ea
“Aaron <github email address>
2021-03-29 17:58:06 -0400

    debugged

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,4 +5,16 @@ class ReviewsController < ApplicationController
         render json: @reviews
     end
 
+    def create
+        @review = Review.create(review_params)
+        render json: @review
+    end
+
+        private
+
+    def review_params
+        params.permit(:date, :review, :rating)
+    end
+
+
 end
